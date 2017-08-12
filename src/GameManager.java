7bae53ec032a7d25349cde7180e563079a6e1e32
flippy84 <ante84@gmail.com>
2017-08-12 19:18:32 +0200

    GameManager started and added threading to it

--- src/GameManager.java
+++ src/GameManager.java
@@ -3,8 +3,37 @@ interface Callback
     abstract void Call();
 }
 
-public class GameManager {
-    GameManager(Player player1, Player player2, Callback callback) {
-        callback.Call();
+public class GameManager extends Thread {
+    private boolean stopped;
+    private Player player1;
+    private Player player2;
+    private GameGrid grid;
+
+    GameManager(Player player1, Player player2, Callback callback, GameGrid grid) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.grid = grid;
+    }
+
+    @Override
+    public void run() {
+        while (!stopped) {
+            Move(player1);
+            Move(player2);
+            System.out.println("Running");
+        }
+        System.out.println("Stopped");
+    }
+
+    public void play() {
+        start();
+    }
+
+    public void quit() {
+        stopped = true;
+    }
+
+    private void Move(Player p) {
+        p.getMove();
     }
 }
--- src/Othello.java
+++ src/Othello.java
@@ -1,5 +1,9 @@
+import javafx.event.EventHandler;
+import javafx.event.WeakEventHandler;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.application.Application;
+import javafx.stage.WindowEvent;
 
 public class Othello extends Application {
     @Override
@@ -7,10 +11,20 @@ public class Othello extends Application {
 
         SetUpGameDialog setup = new SetUpGameDialog();
         if(setup.startGame) {
+            GameGrid grid = new GameGrid();
+            GameFrame frame = new GameFrame(grid);
+            Scene scene = new Scene(frame);
+
             GameManager gm = new GameManager(setup.player1, setup.player2, () -> {
                 System.out.println("Hej");
-            });
+            }, grid);
+            gm.play();
 
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Othello");
+            primaryStage.setOnCloseRequest(event -> {
+                gm.quit();
+            });
             primaryStage.show();
         }
     }
