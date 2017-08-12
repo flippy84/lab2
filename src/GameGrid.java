a208b9261e207dcbbc75fd79f5ddf01b784beaac
flippy84 <ante84@gmail.com>
2017-08-12 19:17:36 +0200

    GameGrid class done

--- src/GameGrid.java
+++ src/GameGrid.java
@@ -1,2 +1,17 @@
+import javafx.util.Pair;
+
 public class GameGrid {
+    private Marker[][] grid;
+
+    public GameGrid() {
+        grid = new Marker[8][8];
+    }
+
+    public void setCell(Marker m, int x, int y) {
+        grid[x % 8][y % 8] = m;
+    }
+
+    public  Marker getCell(int x, int y) {
+        return grid[x % 8][y % 8];
+    }
 }
