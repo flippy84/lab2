9f3bc01f30dd99e76448e532842c03554d26a539
flippy84 <ante84@gmail.com>
2017-08-12 19:16:48 +0200

    GameBoard and GameFrame started

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,22 +1,11 @@
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
-public class GameBoard extends AnchorPane {
-    public GameBoard() throws Exception {
+public class GameBoard extends Pane {
+    public GameBoard(GameGrid grid) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("GameBoard.fxml"));
-        Parent root = loader.load();
-
-        this.getChildren().add(root);
-
-        /*Scene scene = new Scene(root);
-        Stage stage = new Stage();
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.setResizable(false);
-        stage.setTitle("Setup players");
-        stage.setScene(scene);
-        stage.showAndWait();*/
-
-        GameBoardController controller = loader.getController();
+        this.getChildren().add(loader.load());
     }
 }
--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,2 +1,17 @@
-public class GameFrame {
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+
+public class GameFrame extends BorderPane {
+    public GameFrame(GameGrid grid) throws Exception {
+        GameBoard board = new GameBoard(grid);
+        this.setCenter(board);
+
+        Parent toolbar = FXMLLoader.load(getClass().getResource("Toolbar.fxml"));
+        this.setTop(toolbar);
+        this.setBottom(new Label("Hej"));
+    }
 }
