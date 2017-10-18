b2b0b47d1bc426c7c477723b4e3b6c7d0ffba1fb
flippy84 <ante84@gmail.com>
2017-10-18 20:10:12 +0200

    Handled unsafe operation of Optional

--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -5,10 +5,21 @@ import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 import java.util.Optional;
 
+/**
+ * ConnectFour is the main class for the game and extends the JavaFX class Application
+ * and overrides the start method.
+ * @author Andreas Carlsson
+ */
 public class ConnectFour extends Application {
+
+    /**
+     * This method is called
+     * @param primaryStage Supplied stage from JavaFX where our game scene is added.
+     * @throws Exception
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Optional<ButtonType> result;
+        Optional result;
         GameGrid gameGrid = new GameGrid();
         GameBoard gameBoard = new GameBoard(gameGrid);
         // GameBoard implements IHumanPlayerInput to support clicking on the GameBoard
@@ -19,7 +30,7 @@ public class ConnectFour extends Application {
         while (true) {
             result = setup.showAndWait();
 
-            if (result.isPresent() && result.get().getButtonData() == ButtonData.OK_DONE) {
+            if (result.isPresent() && result.get() instanceof ButtonType && ((ButtonType)result.get()).getButtonData() == ButtonData.OK_DONE) {
                 if (!setup.getPlayer1().init() || !setup.getPlayer2().init()) {
                     continue;
                 }
@@ -29,7 +40,7 @@ public class ConnectFour extends Application {
         }
 
         // If the user clicked OK start the game
-        if (result.isPresent() && result.get().getButtonData() == ButtonData.OK_DONE)
+        if (result.isPresent() && result.get() instanceof ButtonType && ((ButtonType)result.get()).getButtonData() == ButtonData.OK_DONE)
         {
             GameManager gameManager = new GameManager(setup.getPlayer1(), setup.getPlayer2(), gameGrid);
             GameFrame gameFrame = new GameFrame(gameManager, gameBoard);
